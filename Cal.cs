65191e3d5a8487d854b2fbbef4358b5775b7f188
minyoung-seo <57670012+minyoung-seo@users.noreply.github.com>
2019-11-26 17:21:23 +0900

    Add files via upload

---
+++ Cal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Cal
+    {
+        public double Plus(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Minus(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Mul(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public int Plus(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Minus(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public float Plus(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Minus(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Mul(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public float Div(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cal MyCal = new Cal();
+            float Num1, Num2, Result = 0;
+            string Sel;
+                        
+            
+            while(true)
+            {
+                Console.WriteLine("두 수의 사칙연산 프로그램");
+                Console.Write("첫번째 수 : ");
+                Num1 = float.Parse(Console.ReadLine());
+                Console.Write("두번째 수 : ");
+                Num2 = float.Parse(Console.ReadLine());
+
+                Console.Write("연산기호를 선택하세요. (+,-,*,/) : ");
+                Sel = Console.ReadLine();
+
+                if (Sel == "+")
+                {
+                    Result = MyCal.Plus(Num1, Num2);
+                    Console.WriteLine("연산결과 : {0}", Result);
+                }
+
+                else if (Sel == "-")
+                {
+                    Result = MyCal.Minus(Num1, Num2);
+                    Console.WriteLine("연산결과 : {0}", Result);
+                }
+
+                else if (Sel == "*")
+                {
+                    Result = MyCal.Mul(Num1, Num2);
+                    Console.WriteLine("연산결과 : {0}", Result);
+                }
+
+                else if (Sel == "/")
+                {
+                    Result = MyCal.Div(Num1, Num2);
+                    Console.WriteLine("연산결과 : {0}", Result);
+                }
+                else
+                    Console.WriteLine("연산기호를 잘못 입력했습니다.");
+
+                if (Sel == "END") break;
+            }
+        }
+    }
+}
